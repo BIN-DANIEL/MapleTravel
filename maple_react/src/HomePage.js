0eed446d910735f660fb348bb5c6c3a406bef5f2
usraptor2016 <wen.347@osu.edu>
2020-10-28 22:18:31 -0500

    Add log in page

--- maple_react/src/HomePage.js
+++ maple_react/src/HomePage.js
@@ -1,10 +1,26 @@
 import React from "react"
-
+import './login.css'
 export default class HomePage extends React.Component {
     constructor(props) {
         super(props)
     }
     render() {
-        return <div>HELLO WORLD</div>
-    }
+        return(
+            <>
+          <div className = "longin">Log in as New User</div> 
+            <div class="login">
+                 <div class="login-triangle">
+                </div>
+                
+             <h2 class="login-header">Log in</h2> 
+
+                <form class="login-container">
+                    <h3><input type="email" placeholder="Email"/></h3>
+                    <p><input type="password" placeholder="Password"/></p>
+                     <p><input type="submit" value="Log in"/></p> 
+                </form>  
+             </div>
+             </>
+        )
+}
 }
