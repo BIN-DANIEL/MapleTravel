3af4fd5083eb0c505dc25f125541d4b3d4f36f36
Yuxuan Liu <liu686@wisc.edu>
2020-10-30 19:04:33 -0500

    added Junit tests
    Added server backend Junit tests

--- maple_spring/src/test/java/com/maple/spring/ApplicationTests.java
+++ maple_spring/src/test/java/com/maple/spring/ApplicationTests.java
@@ -1,13 +1,82 @@
 package com.maple.spring;
 
+import com.maple.spring.dao.AdminDao;
+import com.maple.spring.entity.User;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class ApplicationTests {
+    @Autowired
+    AdminDao adminDao;
+    JdbcTemplate jdbcTemplate;
+    User test = new User();
+    private static final String userTable = "User";
+    private static final String nonexistentUser = "test123456test123456";
+    private static final String normalTestUserName = "test123456";
+    private static final String normalTestPassword = "123456789";
 
-    @Test
-    void contextLoads() {
+    @BeforeClass
+    public void setup() {
+        test.setUsername(normalTestUserName);
+        test.setPassword(normalTestPassword);
+        if(adminDao.hasUser(nonexistentUser)){
+            adminDao.deleteUser(nonexistentUser);
+        }
     }
 
+    @Test
+    public void testCreateUserTable(){
+        String sql = "SELECT COUNT(*) FROM " + userTable;
+        int numOfCars = jdbcTemplate.queryForObject(sql, Integer.class);
+        assertNotEquals(0, numOfCars);
+    }
+
+    @Test
+    public void testHasUserNonExistent() {
+        assertFalse(adminDao.hasUser(nonexistentUser));
+    }
+
+    @Test
+    public void testHasUserExisting() {
+        adminDao.addUser(test);
+        assertTrue(adminDao.hasUser(nonexistentUser));
+    }
+
+    @Test
+    public void testAddUser() {
+        User tempUser = new User();
+        tempUser.setPassword("999999999");
+        tempUser.setUsername("temp");
+        adminDao.addUser(tempUser);
+        assertTrue(adminDao.hasUser(tempUser.getUsername()));
+    }
+
+    @Test
+    public void testAddUserIdempotent() {
+        adminDao.addUser(test);
+        adminDao.addUser(test);
+        assertTrue(adminDao.hasUser(test.getUsername()));
+    }
+
+    @Test
+    public void testDeleteUserIdempotent() {
+        adminDao.addUser(test);
+        adminDao.deleteUser(test.getUsername());
+        assertFalse(adminDao.hasUser(test.getUsername()));
+    }
+
+    @Test
+    public void testDeleteNonExistentUserNoException() {
+        try {
+            adminDao.deleteUser(nonexistentUser);
+        } catch (Exception e){
+            fail();
+        }
+        assertTrue(true);
+    }
 }
