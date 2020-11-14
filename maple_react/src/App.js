fabe62a47b7271b88a52c8f743e4fe2844704532
Jiahui Zhou <jiahuizhou@Jiahuis-MacBook-Pro.local>
2020-11-13 19:35:37 -0600

    router to classroom

--- maple_react/src/App.js
+++ maple_react/src/App.js
@@ -14,6 +14,7 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ResetPassword from './pages/ResetPassword';
+import ClassRoom from './pages/ClassRoom';
 
 function App() {
 
@@ -49,6 +50,9 @@ function App() {
         </Route>
         <Route path="/reset-password">
           <ResetPassword />
+        </Route>
+        <Route path="/classroom">
+          <ClassRoom />
         </Route>
       </Switch>
     </>
